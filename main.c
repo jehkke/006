f902b747119a68cadfdb83b9e4b6d95814fa4229
jehkke <jbndoit@gmail.com>
2023-10-12 15:50:20 +0900

    실습2 ..

--- main.c
+++ main.c
@@ -4,21 +4,22 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 	
-void print_star(void) 
-{
 
-	int i;
-	for(i=0;i<10;i++)
-		printf("*");
-}
+
+
+
+
+
+void func1(void){
+	int x;
+	printf("func1 x is at %p\n",&x);
 	
-	int main(void)
-	{
-		print_star();
-		print_star();
-		print_star();
-
-	system("PAUSE");
+}
+int main (void){
+	int x;
+	printf("main x is at %p\n",&x);
+	func1();
+	
 	return 0;
-	}
-
+	
+}
